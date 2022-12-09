76d101ff364f9c588e2bb0db45b45c33ef3c8ae5
“ronnyrusmayadi” <ronirusmayadi4@gmail.com>
2022-12-09 20:58:14 +0700

    update test

--- test/presentation/bloc/movie/movie_detail_bloc_test.dart
+++ test/presentation/bloc/movie/movie_detail_bloc_test.dart
@@ -3,13 +3,19 @@ import 'package:ditonton/domain/usecases/movies/get_movie_recommendations.dart';
 import 'package:ditonton/domain/usecases/movies/get_watchlist_status.dart';
 import 'package:ditonton/domain/usecases/movies/remove_watchlist.dart';
 import 'package:ditonton/domain/usecases/movies/save_watchlist.dart';
+import 'package:ditonton/presentation/bloc/movie/detail-movie/detail_movie_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 
-@GenerateMocks([
-  GetMovieDetail,
-  GetMovieRecommendations,
-  GetWatchListStatus,
-  SaveWatchlist,
-  RemoveWatchlist,
-])
-void main() {}
+import 'movie_detail_bloc_test.mocks.dart';
+
+@GenerateMocks([GetMovieDetail])
+void main() {
+  late DetailMovieBloc detailMovieBloc;
+  late MockGetMovieDetail mockGetMovieDetail;
+
+  setUp(() {
+    mockGetMovieDetail = MockGetMovieDetail();
+    detailMovieBloc = DetailMovieBloc(mockGetMovieDetail);
+  });
+}
--- test/presentation/bloc/movie/watchlist_bloc_test.dart
+++ test/presentation/bloc/movie/watchlist_bloc_test.dart
@@ -1,5 +1,13 @@
 import 'package:ditonton/domain/usecases/movies/get_watchlist_movies.dart';
+import 'package:ditonton/domain/usecases/movies/get_watchlist_status.dart';
+import 'package:ditonton/domain/usecases/movies/remove_watchlist.dart';
+import 'package:ditonton/domain/usecases/movies/save_watchlist.dart';
 import 'package:mockito/annotations.dart';
 
-@GenerateMocks([GetWatchlistMovies])
+@GenerateMocks([
+  GetWatchlistMovies,
+  GetWatchListStatus,
+  SaveWatchlist,
+  RemoveWatchlist,
+])
 void main() {}
